0d8201f1d1746ca9ce343d0c5e0710d5bab98fe4
rakvat <rakvat@users.noreply.github.com>
2018-09-13 03:43:05 -0700

    refactoring

--- experiment2/planning_alg3/planning.py
+++ experiment2/planning_alg3/planning.py
@@ -10,45 +10,25 @@ class Planning:
         self.input_dir = input_dir
         self.output_dir = output_dir
 
-    def harmony(self, target, netouput):
-        scale = (netouput - target) / target
-        return scale - 0.5 * pow(scale, 2) if scale < 0 else math.log(scale+1)
-
-    def derivative_harmony(self, target, netoutput):
-        epsilon = 0.000001
-        base = self.harmony(target, netoutput)
-        base_plus_epsilon = self.harmony(target, epsilon + netoutput)
-        return (base_plus_epsilon - base) / epsilon
-
-
     def import_data(self):
         self.flows = pd.read_csv(f"{self.input_dir}/flows.csv")
         self.products = list(self.flows)[1:]
         self.num_products = len(self.products)
         rows = list(self.flows['headings'])
         self.row_map = {name: index for index, name in enumerate(rows)}
-
         self.targets = pd.read_csv(f"{self.input_dir}/targets.csv")
         self.years = self.targets.shape[0]
+        # strip year and labor column
+        self.product_targets = np.array(self.targets)[:,1:(self.num_products + 1)].astype(float)
 
     def harmonize(self):
-        # fake it till you make it
-        self.labor_in_year = np.array([10, 9, 8, 7, 8])
-        self.labor_for = np.random.rand(5, self.num_products)
-        self.output_of = 3 * np.random.rand(5, self.num_products) + 2
-        self.productive_consumption_of = np.random.rand(5, self.num_products)
-        self.final_consumption_of = self.output_of - self.productive_consumption_of
+        # random initial production weights
+        self.production_weights = 100 * np.random.rand(5, self.num_products)
+        self.calc_outputs()
+        self.calc_harmony()
+        self.calc_labor()
+        self.calc_target_fulfillment()
 
-        # strip year and labor column
-        product_targets = np.array(self.targets)[:,1:(self.num_products + 1)].astype(float)
-
-        self.target_fulfillment_of = np.divide(
-                self.final_consumption_of,
-                product_targets,
-                out = np.ones_like(self.final_consumption_of),
-                where = product_targets != 0
-        )
-        self.target_fulfillment_in_year = np.sum(self.target_fulfillment_of, axis = 1)
 
     def export_results(self):
         df = pd.DataFrame(self.target_fulfillment_in_year)
@@ -64,6 +44,37 @@ class Planning:
         df = pd.DataFrame(self.output_of, columns=self.products)
         df.to_csv(f"{self.output_dir}/output_of.csv", columns=self.products)
 
+    def harmony(self, target, netouput):
+        scale = (netouput - target) / target
+        return scale - 0.5 * pow(scale, 2) if scale < 0 else math.log(scale+1)
+
+    def derivative_harmony(self, target, netoutput):
+        epsilon = 0.000001
+        base = self.harmony(target, netoutput)
+        base_plus_epsilon = self.harmony(target, epsilon + netoutput)
+        return (base_plus_epsilon - base) / epsilon
+
+
+    def calc_outputs(self):
+        self.output_of = 3 * np.random.rand(5, self.num_products) + 2
+        self.productive_consumption_of = np.random.rand(5, self.num_products)
+        self.final_consumption_of = self.output_of - self.productive_consumption_of
+
+    def calc_harmony(self):
+        return 1
+
+    def calc_target_fulfillment(self):
+        self.target_fulfillment_of = np.divide(
+                self.final_consumption_of,
+                self.product_targets,
+                out = np.ones_like(self.final_consumption_of),
+                where = self.product_targets != 0
+        )
+        self.target_fulfillment_in_year = np.sum(self.target_fulfillment_of, axis = 1)
+
+    def calc_labor(self):
+        self.labor_for = np.random.rand(5, self.num_products)
+        self.labor_in_year = np.sum(self.labor_for, axis = 1)
 
     def __target(self, key, year):
         return float(self.targets[key][year])
